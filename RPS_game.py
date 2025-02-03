8f33d6433c98f10e58e2fea1d750d9efa27ba33e
Anthony Carmona <ac5891358@gmail.com>
2025-02-03 01:06:55 +0000

    finished the game

--- RPS_game.py
+++ RPS_game.py
@@ -69,6 +69,7 @@ def kris(prev_opponent_play):
         prev_opponent_play = "R"
     ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
     return ideal_response[prev_opponent_play]
+    #for pushing 
 
 
 def abbey(prev_opponent_play,
